a1d7f17b8edc56adf9e088bbc91b5b8d16e41daf
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-11-07 19:03:18 +0200

    Fix Docker login

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,7 @@ IMAGE_NAME="${IMAGE_NAME:-python-pipeline-app}"
 COMMIT_HASH="$(git rev-parse --verify HEAD)"
 
 if [ -n "$CI" ]; then
-  echo "$CR_PAT" | docker login ghcr.io -u "$OWNER" --password-stdin
+  docker login ghcr.io -u "$OWNER" -p "$CR_PAT"
 fi
 
 docker build -t "ghcr.io/$OWNER/$IMAGE_NAME:$COMMIT_HASH" .
