797747b2640e4e0b9320717767c882d3f5cc0660
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-11-07 20:21:23 +0200

    Fix local acceptance tests

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -9,7 +9,11 @@ fi
 
 if [ "$ENVIRONMENT_URL" = "$DEFAULT_ENVIRONMENT_URL" ] && ! docker compose ps --filter 'status=running' --services | grep -q app; then
   echo "Starting containers..."
-  docker compose -f docker-compose.yml up -d
+  if [ -n "$CI" ]; then
+    docker compose -f docker-compose.yml up -d
+  else
+    docker compose up -d
+  fi
   _stop_container=1
 fi
 
@@ -19,7 +23,11 @@ _status=$?
 
 if [ -n "$_stop_container" ] || test $_status -ne 0; then
   echo "Stopping containers..."
-  docker compose -f docker-compose.yml down
+  if [ -n "$CI" ]; then
+    docker compose -f docker-compose.yml down
+  else
+    docker compose down
+  fi
 fi
 
 exit $_status
