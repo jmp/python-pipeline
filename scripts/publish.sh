14579954de71c4cb4640a3c51da90e92e4ab94f7
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-11-07 19:05:54 +0200

    Login user docker/login-action

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,9 +4,5 @@ OWNER="${OWNER:-jmp}"
 IMAGE_NAME="${IMAGE_NAME:-python-pipeline-app}"
 COMMIT_HASH="$(git rev-parse --verify HEAD)"
 
-if [ -n "$CI" ]; then
-  docker login ghcr.io -u "$OWNER" -p "$CR_PAT"
-fi
-
 docker build -t "ghcr.io/$OWNER/$IMAGE_NAME:$COMMIT_HASH" .
 docker push "ghcr.io/$OWNER/$IMAGE_NAME:$COMMIT_HASH"
